8f90c6f2204b039a2548223189188885c1f0c6db
Matthew Shelto <mrhenryford59@gmail.com>
2022-03-27 18:50:43 -0400

    added post initialization and some metadata

--- src/main/java/com/luxtechllc/wandcraft/WandCraft.java
+++ src/main/java/com/luxtechllc/wandcraft/WandCraft.java
@@ -4,6 +4,7 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -27,4 +28,8 @@ public class WandCraft
     {
         
     }
+    @EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+        
+	}
 }
