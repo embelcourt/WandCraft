e63f720db2996f2f54537532d51e8e95140ffe21
Evan B <bel190604@gmail.com>
2022-04-11 18:42:33 -0400

    water spawn where you look

--- src/main/java/com/luxtechllc/wandcraft/events/SpawnWater.java
+++ src/main/java/com/luxtechllc/wandcraft/events/SpawnWater.java
@@ -3,6 +3,8 @@ package com.luxtechllc.wandcraft.events;
 import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -12,12 +14,15 @@ public class SpawnWater extends Event{
 	 @SubscribeEvent
 	 public void onClientTick(TickEvent.ClientTickEvent event) {
 		 try {
-			 BlockPos player = Minecraft.getMinecraft().player.getPosition();
+			 RayTraceResult lookingAtobj = Minecraft.getMinecraft().objectMouseOver;
+			 BlockPos lookingatPos = lookingAtobj.getBlockPos();
+			 BlockPos player = new BlockPos(lookingatPos);
+			 
 			 Minecraft.getMinecraft().world.setBlockState(player, Blocks.WATER.getDefaultState());
-			 System.out.println(player);
+			 //System.out.println(player);
 		 }
 		 catch(NullPointerException e){
-			 System.out.println("No player position");
+			 //System.out.println("No player position");
 		 }
 	}
 }
--- src/main/java/com/luxtechllc/wandcraft/init/ModItems.java
+++ src/main/java/com/luxtechllc/wandcraft/init/ModItems.java
@@ -1,13 +1,24 @@
 package com.luxtechllc.wandcraft.init;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.passive.EntityPig;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.Random;
+
+import com.jcraft.jorbis.Block;
 import com.luxtechllc.wandcraft.WandCraft;
 import com.luxtechllc.wandcraft.items.BasicItem;
 
@@ -32,5 +43,12 @@ public class ModItems {
 	
 	private static void registerRender(Item item) {
 		ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation( item.getRegistryName(), "normal"));
+		
+		
+			
+			}
+	
 	}
-}
+				
+		
+	
