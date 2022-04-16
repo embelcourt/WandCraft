ce3a8dcb75343f22bf1ff72d59385dbc9d93bdf1
Matthew Shelto <mrhenryford59@gmail.com>
2022-04-15 22:33:20 -0400

    wand spawns lava where player looks on swing

--- src/main/java/com/luxtechllc/wandcraft/events/SpawnWater.java
+++ src/main/java/com/luxtechllc/wandcraft/events/SpawnWater.java
@@ -2,7 +2,9 @@ package com.luxtechllc.wandcraft.events;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -12,12 +14,17 @@ public class SpawnWater extends Event{
 	 @SubscribeEvent
 	 public void onClientTick(TickEvent.ClientTickEvent event) {
 		 try {
-			 BlockPos player = Minecraft.getMinecraft().player.getPosition();
-			 Minecraft.getMinecraft().world.setBlockState(player, Blocks.WATER.getDefaultState());
-			 System.out.println(player);
+			 if(Minecraft.getMinecraft().player.isSwingInProgress) {
+				 RayTraceResult lookingAtobj = Minecraft.getMinecraft().objectMouseOver;
+				 BlockPos lookingatPos = lookingAtobj.getBlockPos();
+				 BlockPos player = new BlockPos(lookingatPos);
+				 Minecraft.getMinecraft().world.setBlockState(player, Blocks.LAVA.getDefaultState());
+				 Minecraft.getMinecraft().world.spawnParticle(EnumParticleTypes.LAVA, Double.valueOf(lookingatPos.getX()), Double.valueOf(lookingatPos.getY()), Double.valueOf(lookingatPos.getZ()), .5, .5, .5);
+			 }			 
+			 //System.out.println(player);
 		 }
 		 catch(NullPointerException e){
-			 System.out.println("No player position");
+			 //System.out.println("No player position");
 		 }
 	}
 }
