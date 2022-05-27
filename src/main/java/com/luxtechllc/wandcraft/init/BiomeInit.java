33755facf18cc8ca7afe137b91b41266a868b498
Evan B <bel190604@gmail.com>
2022-05-27 17:39:06 -0400

    Update BiomeInit.java

--- src/main/java/com/luxtechllc/wandcraft/init/BiomeInit.java
+++ src/main/java/com/luxtechllc/wandcraft/init/BiomeInit.java
@@ -1,7 +1,8 @@
 package com.luxtechllc.wandcraft.init;
 
 import net.minecraft.world.biome.Biome;
-
+import net.minecraft.
 public class BiomeInit {
 	public static Biome Elder_treebiome;
 }
+private static Biome initBiome(Biome biome, elder_treebiome, BiomeType, biometype,Type type )
