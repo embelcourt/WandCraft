0d4dde903e24e1e59af2d407a6f3e3d57991deac
Evan B <bel190604@gmail.com>
2023-05-25 10:24:03 -0400

    added comments
    I commented out the whole file since there are errors in it until I am able to fix the biome class for now I will keep the code commented out to keep away the errors so that I can code without seeing errors I haven't fixed yet so I can work on other files.

--- src/main/java/com/luxtechllc/wandcraft/init/BiomeInit.java
+++ src/main/java/com/luxtechllc/wandcraft/init/BiomeInit.java
@@ -1,8 +1,9 @@
 package com.luxtechllc.wandcraft.init;
 
-import net.minecraft.world.biome.Biome;
+/* import net.minecraft.world.biome.Biome;
 import net.minecraft.
 public class BiomeInit {
 	public static Biome Elder_treebiome;
 }
 private static Biome initBiome(Biome biome, elder_treebiome, BiomeType, biometype,Type type )
+*/
